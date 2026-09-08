05abd307985645e3d3800bde057ae3942bd1e7ba
Rehan-2439 <bca2540@rajagiri.edu>
2026-09-08 22:33:48 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,26 +1,23 @@
-package moviecollection;
+package loginproject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DatabaseConnection {
 
     private static final String URL =
             "jdbc:mysql://localhost:3306/movie_collection";
+
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "YOUR_MYSQL_PASSWORD"; // Change if your root user has a password
 
-    public static void main(String[] args) {
+    private static final String PASSWORD = "YOUR_MYSQL_PASSWORD";
 
-        try (Connection con =
-                     DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+    public static Connection getConnection() throws Exception {
 
-            System.out.println("Database Connected Successfully!");
-
-        } catch (SQLException e) {
-            System.out.println("Database Connection Failed!");
-            e.printStackTrace();
-        }
+        return DriverManager.getConnection(
+                URL,
+                USERNAME,
+                PASSWORD
+        );
     }
 }
